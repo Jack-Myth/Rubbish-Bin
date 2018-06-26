51b6fc401426d8de1cd0d7be6fdef4a1b609d3e9
JackMyth <wwwbkkk@126.com>
2018-06-26 17:51:53 +0800

    Finish "Inverse Color" Post Process Effect.

--- Advanced OpenGL/FrameBuffer_FragShader.glsl
+++ Advanced OpenGL/FrameBuffer_FragShader.glsl
@@ -14,13 +14,13 @@ out vec4 fragColor;
 void main()
 {
 	vec4 tmpfragColor=vec4(0.f);
-	//fragColor=vec4(vec3(1.0f)-texture(Tex0,aTextureCoord).xyz,1.f);
-	for(float x=-1;x<=1;x++)
+	fragColor=vec4(vec3(1.0f)-texture(Tex0,aTextureCoord).xyz,1.f);
+	/*for(float x=-1;x<=1;x++)
 		for(float y=-1;y<=1;y++)
 		{
 			vec2 xy=vec2(x*offset,y*offset);
 			int i=int((x+1)*3+(y+1));
 			tmpfragColor+=(texture(Tex0,aTextureCoord+xy)*Kernel[i]);
-		}
+		}*/
 	fragColor=tmpfragColor;
 }
