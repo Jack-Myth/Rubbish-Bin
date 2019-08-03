5c045e321cd66b90d1fb2c0e1e69ca8dad0ac379
Jack-Myth <wwwbkkk@126.com>
2019-08-03 19:46:44 +0800

    Add something intresting to make Surface book more useful

---
+++ SurfacePenHelper/SurfacePenHelper.cpp
@@ -0,0 +1,42 @@
+#include <Windows.h>
+#include <stdio.h>
+#include <TlHelp32.h>
+
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd)
+{
+	HWND CurrentWindow = GetForegroundWindow();
+	if (CurrentWindow)
+	{
+		char ClassName[256];
+		do
+		{
+			GetClassNameA(CurrentWindow, ClassName, 255);
+			//MessageBoxA(nullptr, ClassName, "", MB_OK);
+			if (!_stricmp("photoshop", ClassName)||!_stricmp("sflRootWindow",ClassName))
+			{
+				INPUT input[4];
+				memset(input, 0, sizeof(input));
+				input[0].type = input[1].type = input[2].type = input[3].type = INPUT_KEYBOARD;  //设置键盘模式  
+				input[0].ki.wVk = input[3].ki.wVk = VK_CONTROL;
+				input[1].ki.wVk = input[2].ki.wVk = 'Z';
+				input[2].ki.dwFlags = input[3].ki.dwFlags = KEYEVENTF_KEYUP;
+				SendInput(4, input, sizeof(INPUT));
+				return 0;
+			}
+		} while (CurrentWindow = GetParent(CurrentWindow));
+	}
+	else
+	{
+		//MessageBoxA(nullptr, "ActiveWindow为空", "", MB_OK);
+	}
+	//发送截图快捷键
+	INPUT input[6];
+	memset(input, 0, sizeof(input));
+	input[0].type = input[1].type = input[2].type = input[3].type = INPUT_KEYBOARD;  //设置键盘模式  
+	input[4].type = input[5].type = INPUT_KEYBOARD;
+	input[0].ki.wVk = input[3].ki.wVk = VK_LWIN;
+	input[1].ki.wVk = input[4].ki.wVk = VK_SHIFT;
+	input[2].ki.wVk = input[5].ki.wVk = 'S';
+	input[3].ki.dwFlags = input[4].ki.dwFlags =input[5].ki.dwFlags = KEYEVENTF_KEYUP; 
+	SendInput(6, input, sizeof(INPUT));
+}
---
+++ VirtualDesktopAppSwitcher/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsDesktop;
+
+namespace VirtualDesktopAppSwitcher
+{
+
+    class Program
+    {
+        [DllImport("user32.dll", EntryPoint = "ShowWindow", SetLastError = true)]
+        static extern int ShowWindow(IntPtr hWnd, uint nCmdShow);
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+                return;
+            SynchronizationContext.SetSynchronizationContext(new SynchronizationContext());
+            VirtualDesktopProvider.Default.Initialize().Wait();
+            string TargetArgs = "";
+            for (int i = 1; i < args.Length; i++)
+                if (args[i].Contains(" "))
+                    TargetArgs += "\"" + args[i] + "\" ";
+                else
+                    TargetArgs += args[i] + " ";
+            Process targetProcess = Process.Start(args[0], TargetArgs);
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(100);
+                if (targetProcess.MainWindowHandle.ToInt64() != 0)
+                {
+                    VirtualDesktop[] desktops = VirtualDesktop.GetDesktops();
+                    if (desktops.Length < 2)
+                        desktops.Append(VirtualDesktop.Create());
+                    VirtualDesktopHelper.MoveToDesktop(targetProcess.MainWindowHandle, desktops[1]);
+                    //ShowWindow(targetProcess.MainWindowHandle, 1);
+                    return;
+                }
+            }
+            targetProcess.Kill();
+        }
+    }
+}
